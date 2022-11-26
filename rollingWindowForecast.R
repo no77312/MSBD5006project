e9157b8d43ce442a8d7318ca7fbaf71d20029ffb
aliveline <karin411@connect.hku.hk>
2022-11-26 21:04:25 +0800

    add model measurement in rolling window example

--- rollingWindowForecast.R
+++ rollingWindowForecast.R
@@ -1,6 +1,7 @@
 ### Ref: https://cran.r-project.org/web/packages/greybox/vignettes/ro.html
 
 library(greybox)
+library(Metrics)
 
 x <- rnorm(100,100,10)
 
@@ -13,12 +14,17 @@ ourValue <- "pred"
 # ci: if the in-sample window size should be constant. 
 # co: whether the holdout sample window size should be constant.
 returnedValues1 <- ro(x, h=3, origins=8, call=ourCall, value=ourValue)
-
-# Mean Absolute Error
-apply(abs(returnedValues1$holdout - returnedValues1$pred),1,mean,na.rm=TRUE) / mean(returnedValues1$actuals)
-
 plot(returnedValues1)
 
+# Mean Absolute Error
+apply(returnedValues1$holdout,1,mae,predicted=returnedValues1$pred)
+
+# Root Mean Square Error
+apply(returnedValues1$holdout,1,rmse,predicted=returnedValues1$pred)
+
+# scaled Mean Absolute Error
+apply(returnedValues1$holdout,1,mase,predicted=returnedValues1$pred)
+#apply(abs(returnedValues1$holdout - returnedValues1$pred),1,mean,na.rm=TRUE) / mean(returnedValues1$actuals)
 
 # want to change the default parameters of RO, we can ask for example, for non-constant holdout and the constant in-sample:
 returnedValues2 <- ro(x, h=3, origins=8, call=ourCall, value=ourValue, ci=TRUE, co=FALSE)
